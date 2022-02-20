d3db940aef7225bc77951171a88a10b717a52629
devj <power3xz@gmail.com>
2022-02-20 14:36:25 +0900

    tokio::spawn 추가

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,25 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:9090").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
-    let (reader, mut writer) = socket.split();
-
-    let mut reader = BufReader::new(reader);
-    let mut line = String::new();
-
     loop {
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        if bytes_read == 0 {
-            break;
-        }
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
 
-        writer.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+
+                if bytes_read == 0 {
+                    break;
+                }
+
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
